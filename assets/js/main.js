6b987ed04890c415be93378140635a9105cba447
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-11-17 06:22:52 +0600

    Footer Services menu
    Footer Services menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,10 @@
     jQuery(document).ready(function($){
 
 
-        $(".embed-responsive iframe").addClass("embed-responsive-item");
-        $(".carousel-inner .item:first-child").addClass("active");
+        // $(".embed-responsive iframe").addClass("embed-responsive-item");
+        // $(".carousel-inner .item:first-child").addClass("active");
         
-        $('[data-toggle="tooltip"]').tooltip();
+        // $('[data-toggle="tooltip"]').tooltip();
 
 
         //Home page Logo owl carousel
@@ -53,30 +53,6 @@
 		        }
 		    }
 		});
-
-
-		// Portfolio 
-		// $(".portfolio-filter li").on('click', function() {
-		// $(".portfolio-filter li").removeClass("active");
-		// $(this).addClass("active");
-
-		/*var filterValue = $ (this).attr("data-filter");
-		$(".portfolio-list").isotope({
-			filter: filterValue,
-			masonry: {
-				columnwidth: '.col-md-4',
-				horizontalOrder: false,
-			}
-		});
-
-
-		$(".portfolio-list").isotope ({
-			layoutMode: 'masonry',
-			masonry: {
-				columnwidth: '.col-md-3',
-				horizontalOrder: false,
-			}
-		});*/
         
 
 
