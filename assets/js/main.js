9c7e12c4bf1dd466ad39e5547747e8e20e638ed7
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-11-19 22:45:33 +0600

    testimonial css and js updated
    testimonial css and js updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,70 +1,77 @@
-(function ($) {
-	"use strict";
-
-    jQuery(document).ready(function($){
+(function($){
+	"use stict";
 
 
-        // $(".embed-responsive iframe").addClass("embed-responsive-item");
-        // $(".carousel-inner .item:first-child").addClass("active");
-        
-        // $('[data-toggle="tooltip"]').tooltip();
+jQuery(document).ready(function($) {
+	
+	$(".testimonial-list").owlCarousel({
+		items: 3,
+		margin: 30,
+		loop: true,
+		nav: false,
+		navSpeed:1200,
+		dots: true,
+		autoplay: false,
+		autoplaySpeed:1200,
+		responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:2
+	        },
+	        1000:{
+	            items:3
+	        }
+	    }
+	});
+
+	$(".logo-carousel").owlCarousel({
+		margin: 30,
+		loop: true,
+		nav: false,
+		dots: false,
+		autoplay: true,
+		responsive:{
+	        0:{
+	            items:2
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:5
+	        }
+	    }
+	});
 
 
-        //Home page Logo owl carousel
-		$(".logo-carousel").owlCarousel({
-			margin: 30,
-			loop: true,
-			nav: false,
-			dots: false,
-			autoplay: true,
-			responsive:{
-		        0:{
-		            items:2
-		        },
-		        600:{
-		            items:3
-		        },
-		        1000:{
-		            items:5
-		        }
-		    }
+	$(".portfolio-filter li").on('click', function() {
+		$(".portfolio-filter li").removeClass("active");
+		$(this).addClass("active");
+
+		var filterValue = $(this).attr("data-filter");
+
+		$(".portfolio-list").isotope({
+			filter: filterValue,
+			layoutMode: 'masonry',
+			masonry: {
+				columnWidth: '.col-md-3',
+				horizontalOrder: false
+			}
 		});
+	});
+
+	$(".js-modal-btn").modalVideo();
 
 
-		//Testimonial list owl carousel
-		$(".testimonial-list").owlCarousel({
-			items: 3,
-			margin: 30,
-			loop: true,
-			nav: false,
-			navSpeed:1200,
-			dots: true,
-			autoplay: false,
-			autoplaySpeed:1200,
-			responsive:{
-		        0:{
-		            items:1
-		        },
-		        600:{
-		            items:2
-		        },
-		        1000:{
-		            items:3
-		        }
-		    }
-		});
-        
+});
 
 
-    });
+	jQuery(window).load(function(){
+		
+		//Activating Isotop
+		
 
-
-
-
-    jQuery(window).load(function(){
-
-        
-    });
-
-
-});	
+	});
+}(jQuery));
